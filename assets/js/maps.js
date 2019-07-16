208bebd989e7ab7a06b8f7f821e0f481fee6a751
Ubuntu <ubuntu@ip-172-31-86-231.ec2.internal>
2019-07-16 10:57:00 +0000

    adding modals and solving coding errors

--- assets/js/maps.js
+++ assets/js/maps.js
@@ -1,9 +1,9 @@
   var map;
-  var mapModal;
+  var mapPlaces;
   var service;
   var infowindow;
 
-  function initMap() {
+   function initMap() {
     map = new google.maps.Map(document.getElementById('map'), {
       center: { lat: 49.9317211, lng: -6.3187838 },
       zoom: 11
@@ -25,7 +25,7 @@
         map: map,
         label: labels[i % labels.length]
       });
-      
+
       marker.addListener('click', function() {
         manageStyles(i);
       });
@@ -34,44 +34,85 @@
 
     var markerCluster = new MarkerClusterer(map, markers, { imagePath: 'https://developers.google.com/maps/documentation/javascript/examples/markerclusterer/m' });
 
-        var sydney = new google.maps.LatLng(-33.867, 151.195);
 
-        infowindow = new google.maps.InfoWindow();
-
-        mapModal = new google.maps.Map(
-            document.getElementById('mapModal'), {center: sydney, zoom: 15});
-
-        var request = {
-          query: 'Museum of Contemporary Art Australia',
-          fields: ['name', 'geometry'],
-        };
-
-        service = new google.maps.places.PlacesService(mapModal);
-
-        service.findPlaceFromQuery(request, function(results, status) {
-          if (status === google.maps.places.PlacesServiceStatus.OK) {
-            for (var i = 0; i < results.length; i++) {
-              createMarker(results[i]);
-            }
-
-            mapModal.setCenter(results[0].geometry.location);
-          }
-        });
-  }
+    var bounds = new google.maps.LatLngBounds();
+    var placesList = document.getElementById('places');
   
-        function createMarker(place) {
-        var marker = new google.maps.Marker({
-          map: mapModal,
-          position: place.geometry.location
-        });
+    var location = new google.maps.LatLng(49.914134, -6.314372);
 
-        google.maps.event.addListener(marker, 'click', function() {
-          infowindow.setContent(place.name);
-          infowindow.open(mapModal, this);
-        });
+    infowindow = new google.maps.InfoWindow();
+
+    mapPlaces = new google.maps.Map(document.getElementById('mapPlaces'), { center: location, zoom: 15 });
+
+    var request = {
+      location: location,
+      radius: '500',
+      type: ['hotel']
+    };
+
+    service = new google.maps.places.PlacesService(mapPlaces);
+
+    service.nearbySearch(request, function(results, status) {
+      if (status === google.maps.places.PlacesServiceStatus.OK) {
+        for (var i = 0; i < results.length; i++) {
+          createMarker(results[i]);
+          if(i<10)
+            createItemInList(results[i],bounds,placesList,service)
       }
-      
-  function manageStyles(i) {
-    $('#exampleModal').modal('show');
-  }
+        mapPlaces.setCenter(results[0].geometry.location);
+      }
+    });
+    
+  map.fitBounds(bounds);
+}
+
+function createMarker(place) {
+  var image = {
+      url: place.icon,
+      size: new google.maps.Size(71, 71),
+      origin: new google.maps.Point(0, 0),
+      anchor: new google.maps.Point(17, 34),
+      scaledSize: new google.maps.Size(25, 25)
+    };
+    
+  var marker = new google.maps.Marker({
+    map: mapPlaces,
+    icon: image,
+    title: place.name,
+    position: place.geometry.location
+  });
+
+  google.maps.event.addListener(marker, 'click', function() {
+    infowindow.setContent(place.name,place.rating);
+    infowindow.open(mapPlaces, this);
+  });
+}
+
+function createItemInList(place,bounds,placesList,service) {
   
+  var a = document.createElement('a');
+  a.target="_blank";
+  a.textContent = place.name;
+  
+  var request = {
+    placeId: place.place_id,
+    fields: ['website']
+  };
+  
+  service.getDetails(request, function(placeDetail, status) {
+    if (status === google.maps.places.PlacesServiceStatus.OK) {
+      a.href= placeDetail.website;
+    }
+  });
+
+  placesList.appendChild(a);
+  
+  var br = document.createElement("br");
+  placesList.appendChild(br);
+
+  bounds.extend(place.geometry.location);
+}
+
+function manageStyles(i) {
+  $('#myModal').modal('show');
+}
